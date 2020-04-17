e1f450e1eb6d881d06435f7b5b40e0e480be8033
programming.ojisan <programming.ojisan@gmail.com>
2020-04-17 12:57:27 +0000

    Add method require_user_logged_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    
+    include SessionsHelper
+    
+    private
+    
+    def require_user_logged_in
+        unless logged_in?
+            redirect_to login_url
+        end
+    end
+
+    def require_user_logged_out
+        if logged_in?
+            flash[:danger] = "Please log out"
+            redirect_back(fallback_location: root_path)
+        end
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+before_action :require_user_logged_out, only: [:new, :create]
+
   def new
   end
 

6405ff4253e96db44f50cdb549d62fa3b71dc60b
programming.ojisan <programming.ojisan@gmail.com>
2020-05-21 20:57:59 +0900

    create method fetch_stock_info for API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     
     include ApplicationHelper
+    include ConditionsHelper
     include SessionsHelper
     
     private
--- app/helpers/conditions_helper.rb
+++ app/helpers/conditions_helper.rb
@@ -2,4 +2,23 @@ module ConditionsHelper
     def count_favorites(condition)
         condition.favorites.count
     end
+    
+    def set_duration_datalist(price_length)
+	    duration_datalist = [["1年間",1],["2年間",2],["3年間",3],["5年間",5],["10年間",10],["20年間",20]]
+        if price_length >= 245*20
+			duration_datalist.slice(0..5)
+        elsif price_length >= 245*10
+			duration_datalist.slice(0..4)
+		elsif price_length >= 245*5
+    			duration_datalist.slice(0..3)
+		elsif price_length >= 245*3
+    			duration_datalist.slice(0..2)
+		elsif price_length >= 245*2
+    			duration_datalist.slice(0..1)
+		elsif price_length >= 245
+    			duration_datalist.slice(0)
+    	else []
+    	end
+    end
+    
 end

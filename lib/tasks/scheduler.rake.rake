73b2039383950a3ae01929fe6ebeca6712cf3a26
programming.ojisan <programming.ojisan@gmail.com>
2020-05-23 21:08:31 +0900

    adjust for heroku

---
+++ lib/tasks/scheduler.rake.rake
@@ -0,0 +1,58 @@
+desc "This task is called by the Heroku scheduler add-on"
+task :fetch_stock_info => :environment do
+    puts "get_stock_info"
+    puts "it works."
+    
+    benchmark_result = Benchmark.realtime do
+        Stock.all.each do |stock|
+            puts "before price count" 
+                puts Price.where(stock_id: stock.id).count 
+                logger = Logger.new("./api.log")
+                
+                base_url = "https://www.alphavantage.co/query?"
+                my_api_key = "LODEE8NFTIT9TIKT"
+                price_type = "TIME_SERIES_DAILY"
+                my_symbol = stock.code
+                Price.where(stock_id: stock.id).present? ? output_size = "compact" : output_size = "full"
+        
+                params = URI.encode_www_form({function: price_type, symbol: my_symbol, outputsize: output_size, apikey: my_api_key})
+                uri = URI.parse(base_url + params)
+                begin
+                    response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |https|
+                        https.open_timeout = 3
+                        https.read_timeout = 10
+                        https.get(uri.request_uri)
+                    end
+                    
+                    case response
+                    when Net::HTTPSuccess
+                       api_result = JSON.parse(response.body)
+            
+                    when Net::HTTPRedirection
+                        logger.warn("Redirection: code=#{response.code} messsage=#{response.message}")
+                    else
+                        logger.warn("Redirection: code=#{response.code} messsage=#{response.message}")
+                    end
+                    
+                    rescue IOError => e
+                        logger.error(e.message)
+                        logger.error(e.message)
+                    rescue JSON::ParserError => e
+                        logger.error(e.message)
+                    rescue => e
+                        logger.error(e.message)
+                end
+                
+                
+                # DBへの保存処理
+                prices = api_result["Time Series (Daily)"]
+                prices.reverse_each do |key, price|
+                    Price.find_or_create_by(stock_id: stock.id, date: Time.parse(key), price: price.dig("4. close"))
+                end
+                puts "after price count" 
+                puts Price.where(stock_id: stock.id).count 
+            end
+        end
+    puts "処理時間:"
+    puts benchmark_result
+end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,66 +4,68 @@ RSpec.describe ApplicationHelper, type: :helper do
     
     describe "trade simulation" do
         before do
-            @nikkei225 = Stock.create(id: 1,name: "Nikkei225", code: "0001")
+            @nikkei225 = Stock.create(id: 1, name: "Nikkei225", code: "0001")
+            d = Date.new(1993, 5, 1)
             (1..10).each do |n|
                 price = (n*n*100) % 1000 + 100
                 @nikkei225.price.create(
-                    date: "20200401",
+                    date: (d + n),
                     price: price
                 )
             end
         end
     
         context "when buy_condition is +10% and sell_c is +30%, " do
-            it "returns 285500 valuation" do
+            it "returns 2857100 valuation" do
                 stock_id = 1
                 buy_condition = 0.1
                 sell_condition = 0.3
                 duration = 10
                 results = simulation(stock_id,buy_condition,sell_condition,duration)
-                expect(results).to eq [285500, 185500, 2.855]
+                expect(results[0][0]).to eq 2857100
             end            
         end
     
         context "when buy_condition is +30% and sell_c is -30%, " do
-            it "returns 70000 valuation" do
+            it "returns 700000 valuation" do
                 stock_id = 1
                 buy_condition = 0.3
                 sell_condition = -0.3
                 duration = 10
                 results = simulation(stock_id,buy_condition,sell_condition,duration)
-                expect(results).to eq [70000, -30000, 0.7]
+                expect(results[0][0]).to eq 700000
             end            
         end
     
         context "when buy_condition is -20% and sell_c is +30%, " do
-            it "returns 28600 valuation" do
+            it "returns 286000 valuation" do
                 stock_id = 1
                 buy_condition = -0.2
                 sell_condition = 0.3
                 duration = 10
                 results = simulation(stock_id,buy_condition,sell_condition,duration)
-                expect(results).to eq [28600, -71400, 0.286]
+                expect(results[0][0]).to eq 286000
             end            
         end
     
         context "when buy_condition is -30% and sell_c is -50%, " do
-            it "returns 285500 valuation" do
+            it "returns 357200 valuation" do
                 stock_id = 1
                 buy_condition = -0.3
                 sell_condition = -0.5
                 duration = 10
                 results = simulation(stock_id,buy_condition,sell_condition,duration)
-                expect(results).to eq [35800, -64200, 0.358]
+                expect(results[0][0]).to eq 357200
             end            
         end
     
         context "when duration is 20, " do
-            it "returns 8000 valuation" do
+            it "returns 80000 valuation" do
+            d = Date.new(1993, 5, 1)
                 (11..20).each do |n|
                     price = (n*n*100) % 1000 + 100
                     @nikkei225.price.create(
-                        date: "20200401",
+                        date: (d + n),
                         price: price
                     )
                 end
@@ -73,19 +75,19 @@ RSpec.describe ApplicationHelper, type: :helper do
                 sell_condition = 1.0
                 duration = 20
                 results = simulation(stock_id,buy_condition,sell_condition,duration)
-                expect(results).to eq [8000, -92000, 0.08]
+                expect(results[0][0]).to eq 80000
             end            
         end
     
         context "when doesn't match any conditions, " do
-            it "returns 100000 valuation" do
+            it "returns 1000000 valuation" do
         
                 stock_id = 1
                 buy_condition = 50000.0
                 sell_condition = -50000.0
                 duration = 10
                 results = simulation(stock_id,buy_condition,sell_condition,duration)
-                expect(results[0]).to eq 100000
+                expect(results[0][0]).to eq 1000000
             end            
         end
     
@@ -96,7 +98,8 @@ RSpec.describe ApplicationHelper, type: :helper do
                 sell_condition = -0.6
                 duration = 10
                 
-                expect(simulation(stock_id,buy_condition,sell_condition,duration)).to include "couldn't find"
+                e = simulation(stock_id,buy_condition,sell_condition,duration)
+                expect(e).to include "Couldn't find"
             end
         end
     end
